0bf86c64bf19b98da57fbe53c87c760f928f212a
Maryam Alrubaye <maryamalrubaye@dizaynvip.com>
2023-01-13 15:47:54 +0300

    manage loading data from the server by the controller.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:iau_task_manager/controllers/data_controller.dart';
 import 'package:iau_task_manager/pages/start.dart';
 
 void main() {
@@ -8,10 +9,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  loadData() async {
+    await Get.find<DataController>().getData();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.lazyPut(() => DataController());
+    loadData();
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'IAU Task Manager',
